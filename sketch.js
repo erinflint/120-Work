8b3d2b3071ca8f2e84526881520c60fe8ad34e4d
Erin Flint <42758837+erinflint@users.noreply.github.com>
2018-10-17 21:53:36 -0600

    HW*8

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
-function setup(){
-  //create canvas size 600px wide and 400px high
-  createCanvas( 600, 400 );
 
-//color background yellow
-background( 'yellow' );
+function setup() {
+  createCanvas( windowWidth, 600 );
 }
-function draw(){
-//tell the program to write "Hello World" at 50px by 60px
-text( "Hello World", 50, 60 );
+
+function draw() {
+  background( 'pink');
+  strokeWeight( 6 );
+//make fish scale-esque circles
+for (var x = 0; x <= mouseX; x += 40){
+  for(var y = 10; y<= mouseY; y += 20){
+  fill( random(255), 0, random(255));
+  ellipse (x + y, y, 8-y/6, 8-y/6);
+  }
+}
+//create moving circles
+fill('purple');
+ellipse(200, mouseX, 60);
+fill(200);
+ellipse(40, mouseY, 60);
+fill('blue');
+ellipse(400, mouseX, 60);
+fill('pink');
+ellipse(600, mouseY, 60);
+fill('yellow');
+ellipse(mouseX, mouseY, 50, 50);
+fill(0);
+//create mouse "target"
+ellipse(900, mouseX, 60);
+ellipse(mouseX, mouseY, 20, 20);
+}
 }
