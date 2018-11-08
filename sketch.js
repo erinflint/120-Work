7fa7df4364f4db92d064595edcc2f421700c9756
Erin Flint <42758837+erinflint@users.noreply.github.com>
2018-11-08 00:22:56 -0700

    HW-11

--- sketch.js
+++ sketch.js
@@ -1,32 +1,61 @@
+let circle = [];
+let x;
+let y;
+let size;
+let color;
+let num_of_circles = 30;
 
 function setup() {
-  createCanvas( windowWidth, 600 );
-}
+    createCanvas(windowWidth, windowHeight);
 
-function draw() {
-  background( 'pink');
-  strokeWeight( 6 );
-//make fish scale-esque circles
-for (var x = 0; x <= mouseX; x += 40){
-  for(var y = 10; y<= mouseY; y += 20){
-  fill( random(255), 0, random(255));
-  ellipse (x + y, y, 8-y/6, 8-y/6);
+    circle = new Circle (random(windowWidth), random(windowHeight), 30);
+
+    for (let i = 0; i < num_of_circles; i++ ) {
+        circle[i] = new Circle(random(windowWidth), random(windowHeight), 30);
+      }
+    }
+
+function draw () {
+  background(0);
+  for( i = 0; i < num_of_circles; i++ ){
+    circle[i].frame();
   }
-}
-//create moving circles
-fill('purple');
-ellipse(200, mouseX, 60);
-fill(200);
-ellipse(40, mouseY, 60);
-fill('blue');
-ellipse(400, mouseX, 60);
-fill('pink');
-ellipse(600, mouseY, 60);
-fill('yellow');
-ellipse(mouseX, mouseY, 50, 50);
-fill(0);
-//create mouse "target"
-ellipse(900, mouseX, 60);
-ellipse(mouseX, mouseY, 20, 20);
-}
+  }
+
+class Circle {
+  constructor( x, y, size, 0 ) {
+    this.shade = 255;
+    this.size = size;
+    this.rad = this.size / 4;
+    this.posX = random(width);
+    this.posY = random(height);
+    this.deltaX = random(-4, 4);
+    this.deltaY = random(-4, 4);
+
+  }
+  frame(){
+    this.display();
+    this.move();
+    this.edgeCheck();
+  }
+  display() {
+    push();
+    noStroke();
+    fill(this.shade);
+    translate( this.posX, this.posY );
+    ellipse( 0, 0, this.size );
+    pop();
+  }
+  move() {
+    this.posX += this.deltaX;
+    this.posY += this.deltaY;
+  }
+  edgeCheck() {
+    if (this.posX + this.rad >= width || this.posX - this.rad <= 0) {
+      this.deltaX *= -1;
+    }
+    if (this.posY + this.rad >= height || this.posY - this.rad <= 0) {
+      this.deltaY *= -1;
+    }
+  }
 }
